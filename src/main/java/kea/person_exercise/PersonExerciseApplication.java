f698bdeac43e24f0878ddaef177b1a79a9063e58
YawHB <yawhboateng@gmail.com>
2024-02-12 10:34:49 +0100

    Initial commit

---
+++ src/main/java/kea/person_exercise/PersonExerciseApplication.java
@@ -0,0 +1,13 @@
+package kea.person_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonExerciseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kea/person_exercise/model/Person.java
@@ -0,0 +1,50 @@
+package kea.person_exercise.model;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Person {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    private LocalDate dateOfBirth;
+}
